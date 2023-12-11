82a50c1540fd84585b31f9b3d028260f2474639b
maziogra <104913994+maziogra@users.noreply.github.com>
2023-12-11 20:40:50 +0100

    Created AuthorDTO

---
+++ src/main/java/com/maziogra/book_api/domain/DTO/AuthorDTO.java
@@ -0,0 +1,16 @@
+package com.maziogra.book_api.domain.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthorDTO {
+    private Long id;
+    private String name;
+    private Integer age;
+}
