79d9e124d02d840bb6dece215c068423a0ac0e45
maziogra <104913994+maziogra@users.noreply.github.com>
2023-12-12 17:45:28 +0100

    Refactored AuthorController.java

--- src/main/java/com/maziogra/book_api/controllers/AuthorController.java
+++ src/main/java/com/maziogra/book_api/controllers/AuthorController.java
@@ -6,14 +6,12 @@ import com.maziogra.book_api.mappers.mapperImpl.AuthorMapper;
 import com.maziogra.book_api.services.impl.AuthorServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/authors")
 public class AuthorController {
     private final AuthorMapper authorMapper;
     private final AuthorServiceImpl authorServiceImpl;
@@ -23,13 +21,13 @@ public class AuthorController {
         this.authorServiceImpl = authorServiceImpl;
     }
 
-    @PostMapping(path = "/authors")
+    @PostMapping
     public ResponseEntity<AuthorDTO> createAuthor(@RequestBody AuthorDTO authorDTO){
         AuthorEntity authorEntity = authorServiceImpl.save(authorMapper.mapFrom(authorDTO));
         return new ResponseEntity<>(authorMapper.mapTo(authorEntity), HttpStatus.OK);
     }
 
-    @GetMapping(path = "/authors")
+    @GetMapping
     public List<AuthorDTO> getAuthors(){
         List<AuthorEntity> authors = authorServiceImpl.getAuthors();
         return authors.stream().map(authorMapper::mapTo).toList();
